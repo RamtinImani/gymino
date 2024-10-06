650ae89ab8f3da3a98509993c782412e55ab2e5b
Ramtin Imani <ramtinimani.dev@gmail.com>
2024-10-06 23:49:25 +0330

    added menu js

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/*=============== SHOW MENU ===============*/
+const toggleBtn = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+const closeBtn = document.getElementById("nav-close");
+// show menu
+if (toggleBtn) {
+  toggleBtn.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+// hidden menu
+if (closeBtn) {
+  closeBtn.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll(".nav__link");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+});
