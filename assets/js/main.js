caa165e1adc4bbb60b52e914d58a4b5934edc2ca
Ramtin Imani <ramtinimani.dev@gmail.com>
2024-10-06 23:50:37 +0330

    added change background header js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,12 @@ navLinks.forEach((link) => {
     navMenu.classList.remove("show-menu");
   });
 });
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
