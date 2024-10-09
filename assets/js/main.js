e9eaf7d4912d994d79cf71f2a83c52ea487c66b5
Ramtin Imani <ramtinimani.dev@gmail.com>
2024-10-09 10:30:22 +0330

    added calculate BMI js

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,55 @@ const scrollActive = () => {
   });
 };
 window.addEventListener("scroll", scrollActive);
+/*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById("calculate-form"),
+  calculateCm = document.getElementById("calculate-cm"),
+  calculateKg = document.getElementById("calculate-kg"),
+  calculateMessage = document.getElementById("calculate-message");
+
+const calculateBmi = (e) => {
+  e.preventDefault();
+
+  // Check if the fields have a value
+  if (calculateCm.value === "" || calculateKg.value === "") {
+    // Add and remove color
+    calculateMessage.classList.remove("color-green");
+    calculateMessage.classList.add("color-red");
+
+    // Show message
+    calculateMessage.textContent = "Fill in the Height and Weight 🫤";
+
+    // Remove message three seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
+  } else {
+    // BMI Formula
+    const cm = calculateCm.value / 100,
+      kg = calculateKg.value,
+      bmi = Math.round(kg / (cm * cm));
+
+    // Show your health status
+    if (bmi < 18.5) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny 😞`;
+    } else if (bmi < 25) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy 🥳`;
+    } else {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight 🫤`;
+    }
+
+    // To clear the input field
+    calculateCm.value = "";
+    calculateKg.value = "";
+
+    // Remove message after 5 seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 5000);
+  }
+};
+
+calculateForm.addEventListener("submit", calculateBmi);
